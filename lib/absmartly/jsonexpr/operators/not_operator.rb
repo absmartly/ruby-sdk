859463014c14f3ce24b0ce81b6debc54a0051833
umairrazam <omairrr.azam@gmail.com>
2022-08-24 22:14:55 +0500

    Not Operator

--- lib/absmartly/jsonexpr/operators/not_operator.rb
+++ lib/absmartly/jsonexpr/operators/not_operator.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
+require_relative 'unary_operator'
 
 module Absmartly
   module Jsonexpr
     module Operators
-      class NotOperator
-        def initialize
+      class NotOperator < UnaryOperator
+        def unary(evaluator, arg)
+          !evaluator.boolean_convert(arg)
         end
       end
     end
